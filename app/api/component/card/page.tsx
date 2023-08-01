a5195584fe9289073e92d3408c1c45fa172db430
ttp12345 <ttp12345@thirautech.com>
2023-08-01 15:36:17 +0900

    1

--- app/api/component/card/page.tsx
+++ app/api/component/card/page.tsx
@@ -2,33 +2,34 @@
 import { useRef } from "react";
 
 import Card from '@mui/material/Card';
+import Grid from '@mui/material/Grid';
 import CardActionArea from '@mui/material/CardActionArea';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 
-export default function Cards(props:any) {
+export default function Cards(props: any) {
 
   const ref = useRef(null);
 
   return (
     <Card ref={ref}>
-    <CardActionArea>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="h2">
-          {props.children}
-        </Typography>
-      </CardContent>
-    </CardActionArea>
-    <CardActions>
-      <Button size="small" color="primary">
-        Share
-      </Button>
-      <Button size="small" color="primary">
-        Learn More
-      </Button>
-    </CardActions>
-  </Card>
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.config.name}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,27 @@
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Unstable_Grid2';
 import Cards from './api/component/card/page'
+
 
 export default function Home() {
   return (
-    <Box component="main">
-      <Cards />
+    <Box component="main" sx={{ marginTop: "50px" }}>
+      <Grid container spacing={1} direction="column"
+        justifyContent="center"
+        alignItems="center" >
+        <Grid sx={{width:"500px"}}>
+          <Cards config={{id:1,name:"메뉴1",useYn:"Y"}} />
+        </Grid>
+        <Grid sx={{width:"500px"}}>
+          <Cards config={{id:2,name:"메뉴2",useYn:"Y"}} />
+        </Grid>
+        <Grid sx={{width:"500px"}}>
+          <Cards config={{id:3,name:"메뉴3",useYn:"Y"}} />
+        </Grid>
+        <Grid sx={{width:"500px"}}>
+          <Cards config={{id:4,name:"메뉴4",useYn:"Y"}} />
+        </Grid>
+      </Grid>
     </Box>
   )
 }
