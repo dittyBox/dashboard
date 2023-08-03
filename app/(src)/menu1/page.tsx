b6a55b358142cccefadb6e3ac56f442fff4a0327
ttp12345 <ttp12345@thirautech.com>
2023-08-03 11:33:14 +0900

    timer add

--- app/(src)/menu1/page.tsx
+++ app/(src)/menu1/page.tsx
@@ -4,7 +4,7 @@ import Grid from '@mui/material/Unstable_Grid2';
 export default function Menu1() {
   return (
     <Box component="main" sx={{ marginTop: "50px" }}>
-      <div>asdasdasdsdasdasdasd</div>
+      <div>111111111111111111111111111111111111111111111</div>
     </Box>
   )
 }
--- app/(src)/menu2/page.tsx
+++ app/(src)/menu2/page.tsx
@@ -4,7 +4,7 @@ import Grid from '@mui/material/Unstable_Grid2';
 export default function Menu2() {
   return (
     <Box component="main" sx={{ marginTop: "50px" }}>
-      <div>asdasdasdsdasdasdasd</div>
+      <div>222222222222222222222222222222</div>
     </Box>
   )
 }
